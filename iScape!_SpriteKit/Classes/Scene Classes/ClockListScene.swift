0fb70eda3d3b17748db83d82bc6a1c48330c71ee
Erica Suguimoto <judaikawa@gmail.com>
2018-08-19 13:45:25 -0300

    Clock app

---
+++ iScape!_SpriteKit/Classes/Scene Classes/ClockListScene.swift
@@ -0,0 +1,146 @@
+//
+//  ClockListScene.swift
+//  iScape!_SpriteKit
+//
+//  Created by Erica Suguimoto on 18/08/2018.
+//  Copyright © 2018 Juliana Daikawa. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ClockListScene: SKScene {
+    
+    // Buttons
+    var upButton: SKShapeNode?
+    var downButton: SKShapeNode?
+    var rightButton: SKShapeNode?
+    var leftButton: SKShapeNode?
+    var selectButton: SKShapeNode?
+    var startButton: SKShapeNode?
+    var aButton: SKShapeNode?
+    var bButton: SKShapeNode?
+    
+    // Direction Buttons
+    var dirButtons = [SKShapeNode?]()
+    var dirButtonsName = [String]()
+    
+    // Menu Buttons
+    var menuButtons = [SKShapeNode?]()
+    var menuButtonsName = [String]()
+    
+    // TableView
+    var tableViewNode: SKSpriteNode?
+    var navBarNode: SKSpriteNode?
+    let tableView = UITableView()
+    
+    weak var myDelegate: StateChosenDelegate?
+    
+    override func didMove(to view: SKView) {
+        
+        setUpButtons()
+        
+        // TableView
+        tableViewNode = self.childNode(withName: "tableViewNode") as? SKSpriteNode
+        navBarNode = self.childNode(withName: "navBarNode") as? SKSpriteNode
+        tableView.frame = CGRect(x: 0, y: 10+(navBarNode?.frame.height)!, width: (tableViewNode?.frame.width)!, height: (tableViewNode?.frame.height)!)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .black
+        self.scene?.view?.addSubview(tableView)
+        
+    }
+    
+}
+
+extension ClockListScene: UITableViewDataSource, UITableViewDelegate {
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TimeZone.knownTimeZoneIdentifiers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: "listCell")
+        if cell == nil {
+            cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "listCell")
+        }
+        
+        let componentsArray = TimeZone.knownTimeZoneIdentifiers[indexPath.row].components(separatedBy: "/")
+        var city = componentsArray[componentsArray.count-1]
+        if city.contains("_") {
+            city = city.replacingOccurrences(of: "_", with: " ")
+        }
+        let continent = componentsArray[0]
+        
+        cell!.textLabel?.text = "\(city), \(continent)"
+        cell!.textLabel?.textColor = .white
+        cell!.textLabel?.font = UIFont(name: pixelArtFontName, size: 10)
+        cell!.backgroundColor = .clear
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let componentsArray = TimeZone.knownTimeZoneIdentifiers[indexPath.row].components(separatedBy: "/")
+        var city = componentsArray[componentsArray.count-1]
+        if city.contains("_") {
+            city = city.replacingOccurrences(of: "_", with: " ")
+        }
+        
+        self.myDelegate?.stateChosenInList(stateIdentifier: TimeZone.knownTimeZoneIdentifiers[indexPath.row], city: city)
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let scene = ClockScene(fileNamed: "ClockScene") {
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            tableView.removeFromSuperview()
+            self.view?.presentScene(scene)
+        }
+        
+        
+        
+    }
+    
+}
+
+// Console View
+extension ClockListScene {
+    func setUpButtons() {
+        
+        dirButtons = [upButton, downButton, rightButton, leftButton]
+        dirButtonsName = ["upButton", "downButton", "rightButton", "leftButton"]
+        
+        menuButtons = [selectButton, startButton]
+        menuButtonsName = ["selectButton", "startButton"]
+        
+        for i in 0..<dirButtons.count {
+            roundButton(button: dirButtons[i], withName: dirButtonsName[i], cornerRadius: 5)
+        }
+        
+        for i in 0..<menuButtons.count {
+            roundButton(button: menuButtons[i], withName: menuButtonsName[i], cornerRadius: 12)
+        }
+    }
+    
+    func roundButton(button: SKShapeNode?, withName name: String, cornerRadius: CGFloat) {
+        if let button = self.childNode(withName: "Console")?.childNode(withName: name) as? SKShapeNode {
+            
+            let roundButton = SKShapeNode(rect: button.frame, cornerRadius: cornerRadius)
+            roundButton.fillColor = button.fillColor
+            roundButton.strokeColor = button.strokeColor
+            self.childNode(withName: "Console")?.addChild(roundButton)
+            button.removeFromParent()
+            roundButton.name = name
+            roundButton.zPosition = 2
+            
+        }
+    }
+}
--- iScape!_SpriteKit/Classes/Scene Classes/ClockScene.swift
+++ iScape!_SpriteKit/Classes/Scene Classes/ClockScene.swift
@@ -8,7 +8,7 @@
 
 import SpriteKit
 
-class ClockScene: SKScene {
+class ClockScene: SKScene, StateChosenDelegate {
     
     // Buttons
     var upButton: SKShapeNode?
@@ -35,9 +35,34 @@ class ClockScene: SKScene {
     
     var characterTextLabelNode: SKLabelNode?
     
+    var titleNode: SKLabelNode?
+    var addButtonNode: SKSpriteNode?
+    
+    var detailViewController: UIViewController?
+    var stateArray = ["Sao Paulo", "San Jose"]
+    weak var timer: Timer?
+    var dateArray = ["", ""]
+    var currentStateDateString = ["", ""]
+    var isPointShown = true
+    var identifierArray = ["America/Sao_Paulo","America/Los_Angeles"]
+    let clockList = ClockListScene()
+    
+    func stateChosenInList(stateIdentifier: String?, city: String?) {
+        stateArray.append(city!)
+        dateArray.append("")
+        identifierArray.append(stateIdentifier!)
+        currentStateDateString.append("")
+        tableView.reloadData()
+    }
+    
     override func didMove(to view: SKView) {
         
+        clockList.myDelegate = self
+        
         setUpButtons()
+        
+        titleNode = self.childNode(withName: "titleNode") as? SKLabelNode
+        addButtonNode = self.childNode(withName: "addButtonNode") as? SKSpriteNode
         
         // Character text
         characterTextLabelNode = self.childNode(withName: "grayViewNode")?.childNode(withName: "baloonNode")?.childNode(withName: "characterTextLabelNode") as? SKLabelNode
@@ -45,6 +70,65 @@ class ClockScene: SKScene {
         characterTextLabelNode?.preferredMaxLayoutWidth = 230
         SKLabelNode.animateText(label: characterTextLabelNode!, newText: "Woah it's getting late! I need to go home...", characterDelay: characterTextDelay)
         
+        // Date
+        setupDate()
+        
+        // Update Time
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(setupDate), userInfo: nil, repeats: true)
+        
+        // TableView
+        tableViewNode = self.childNode(withName: "tableViewNode") as? SKSpriteNode
+        navBarNode = self.childNode(withName: "navBarNode") as? SKSpriteNode
+        tableView.frame = CGRect(x: 0, y: 10+(navBarNode?.frame.height)!, width: (tableViewNode?.frame.width)!, height: (tableViewNode?.frame.height)!)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .black
+        self.scene?.view?.addSubview(tableView)
+        
+    }
+    
+    deinit {
+        timer?.invalidate()
+    }
+    
+    @objc func addTimeZone() {
+        
+        // Present next scene
+        if let scene = ClockListScene(fileNamed: "ClockListScene") {
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            tableView.removeFromSuperview()
+            self.view?.presentScene(scene)
+        }
+        
+    }
+    
+    @objc private func setupDate() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        
+        for i in 0..<stateArray.count {
+            
+            formatter.timeZone = TimeZone(identifier: identifierArray[i])
+            currentStateDateString[i] = formatter.string(from: Date())
+        }
+        
+        if isPointShown {
+            for j in 0..<stateArray.count {
+                dateArray[j] = currentStateDateString[j].replacingOccurrences(of: ":", with: " ")
+            }
+            isPointShown = !isPointShown
+        } else {
+            for j in 0..<stateArray.count {
+                dateArray[j] = currentStateDateString[j].replacingOccurrences(of: " ", with: ":")
+            }
+            isPointShown = !isPointShown
+        }
+        
+        
+        tableView.reloadData()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -54,6 +138,8 @@ class ClockScene: SKScene {
             let location = touch.location(in: self)
             
             switch atPoint(location).name {
+            case "addButtonNode":
+                addTimeZone()
             case "bButton":
                 if let scene = MainScene(fileNamed: "MainScene") {
                     // Set the scale mode to scale to fit the window
@@ -74,6 +160,64 @@ class ClockScene: SKScene {
     }
 
 }
+
+extension ClockScene: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stateArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = ClockCell(style: .default, reuseIdentifier: "clockCell")
+        
+        cell.stateLabel.text = stateArray[indexPath.row]
+        cell.timeLabel.text = dateArray[indexPath.row]
+        cell.backgroundColor = .clear
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    
+}
+
+class ClockCell: UITableViewCell {
+    
+    let stateLabel = UILabel()
+    let timeLabel = UILabel()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // State Label
+        stateLabel.frame = CGRect(x: 16, y: 7.5, width: 300, height: 45)
+        stateLabel.font = UIFont(name: pixelArtFontName, size: 15)
+        stateLabel.textColor = .white
+        contentView.addSubview(stateLabel)
+        
+        // Time Label
+        timeLabel.frame = CGRect(x: 260, y: 10, width: 200, height: 40)
+        timeLabel.font = UIFont(name: pixelArtFontName, size: 20)
+        timeLabel.textColor = .white
+        contentView.addSubview(timeLabel)
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+protocol StateChosenDelegate: class {
+    func stateChosenInList(stateIdentifier: String?, city: String?)
+}
+
 
 // Console View
 extension ClockScene {
